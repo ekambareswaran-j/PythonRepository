58bae42f9c9eb81144f046f87df686f343b31b4d
ekambareswaran-j <36337990+ekambareswaran-j@users.noreply.github.com>
2020-09-18 20:08:05 +0530

    Add files via upload

---
+++ Patient.py
@@ -0,0 +1,10 @@
+name='John'
+age=20
+type='new'
+is_new=True
+
+print('Name=',name)
+print('Age=',age)
+print('Type=',type)
+print('Is_NEW=', is_new)
+
---
+++ Pound2Kg.py
@@ -0,0 +1,5 @@
+weight = input('Please enter your weight in pounds: ')
+print(type(weight))
+weight_in_kg = float(weight) *0.453592
+# print('Weight in kg: ', float(weight) * 0.453592)
+print('Weight in Kg = ',round(weight_in_kg))
